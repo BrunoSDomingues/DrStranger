3fea62c3c42ef6ddc8accebf0727eec6618edf28
Bruno Domingues <brunosd1@al.insper.edu.br>
2022-05-10 18:47:48 -0300

    Changes in laser script

--- Assets/Scripts/Laser.cs
+++ Assets/Scripts/Laser.cs
@@ -8,12 +8,14 @@ public class Laser : MonoBehaviour
     private GameObject bullet;
 
     private float offsetX = 0.4f, offsetY = 0.7f, offsetZ = 1.6f;
-    private Vector3 speed;
-    public static float multiplier = 1000f;
+    private Vector3 prevSpeed, impulse;
+    private Rigidbody rb;
+    public static bool timeStop = false;
 
     // Start is called before the first frame update
     void Start()
     {
+        impulse = transform.forward * 1500f;
         StartCoroutine(FireLaser());
     }
 
@@ -22,21 +24,29 @@ public class Laser : MonoBehaviour
     {
         while (true)
         {
-            speed = transform.forward * multiplier;
-            bullet = Instantiate(
-                prefab, 
-                new Vector3(
-                    transform.position.x + offsetX, 
-                    transform.position.y + offsetY, 
-                    transform.position.z + offsetZ
-                ),
-                Quaternion.identity
-            );
-            bullet.transform.Rotate(new Vector3(90, 0, 0));
-            bullet.GetComponent<Rigidbody>().AddForce(speed);
+            if (!timeStop)
+            {
+                bullet = Instantiate(
+                    prefab,
+                    new Vector3(
+                        transform.position.x + offsetX,
+                        transform.position.y + offsetY,
+                        transform.position.z + offsetZ
+                    ),
+                    Quaternion.identity
+                );
+            }
+            if (bullet)
+            {
+                rb = bullet.GetComponent<Rigidbody>();
+                bullet.transform.Rotate(new Vector3(90, 0, 0));
+
+                if (!timeStop) rb.AddForce(impulse);
+                else rb.velocity = Vector3.zero;
+                Destroy(bullet, 3f);
+                offsetX = offsetX * (-1);
+            }
             yield return new WaitForSeconds(3);
-            Destroy(bullet);
-            offsetX = offsetX * (-1);
         }
     }
 }
