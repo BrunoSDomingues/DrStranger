6c2b482b55dde19212da85ca11d076f1d7eda70a
Bruno Domingues <brunosd1@al.insper.edu.br>
2022-06-02 17:54:04 -0300

    Edited more scripts

--- Assets/Scripts/Laser.cs
+++ Assets/Scripts/Laser.cs
@@ -2,49 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Laser : MonoBehaviour
-{
+public class Laser : MonoBehaviour {
     public GameObject prefab;
-    private GameObject bullet;
-
-    private float offsetX = 0.4f, offsetY = 0.7f, offsetZ = 1.6f;
-    private Vector3 prevSpeed, impulse;
-    private Rigidbody rb;
     public static bool timeStop = false;
 
-    // Start is called before the first frame update
-    void Start()
-    {
+    private GameObject bullet, player;
+
+    private Vector3 impulse;
+    private Rigidbody rb;
+
+
+    void OnEnable() {
+        player = GameObject.FindWithTag("Player");
         impulse = transform.forward * 1500f;
         StartCoroutine(FireLaser());
     }
 
-    // Update is called once per frame
-    IEnumerator FireLaser()
-    {
-        while (true)
-        {
-            if (!timeStop)
-            {
-                bullet = Instantiate(
-                    prefab,
-                    new Vector3(
-                        transform.position.x + offsetX,
-                        transform.position.y + offsetY,
-                        transform.position.z + offsetZ
-                    ),
-                    Quaternion.identity
-                );
-            }
-            if (bullet)
-            {
-                rb = bullet.GetComponent<Rigidbody>();
-                bullet.transform.Rotate(new Vector3(90, 0, 0));
+    IEnumerator FireLaser() {
+        while (true) {
+            float dist = Vector3.Distance(player.transform.position, transform.position);
+            if (dist <= 25) {
+                if (!timeStop) {
+                    bullet = Instantiate(
+                        prefab,
+                        transform.position,
+                        Quaternion.identity
+                    );
+                }
+                if (bullet) {
+                    rb = bullet.GetComponent<Rigidbody>();
+                    bullet.transform.Rotate(new Vector3(90, 0, 0));
 
-                if (!timeStop) rb.AddForce(impulse);
-                else rb.velocity = Vector3.zero;
-                Destroy(bullet, 3f);
-                offsetX = offsetX * (-1);
+                    if (!timeStop) rb.AddForce(impulse);
+
+                    else rb.velocity = Vector3.zero;
+                    Destroy(bullet, 3f);
+                }
             }
             yield return new WaitForSeconds(3);
         }
--- Assets/Scripts/LeftController.cs
+++ Assets/Scripts/LeftController.cs
@@ -2,8 +2,7 @@ using UnityEngine;
 using Valve.VR.Extras;
 using Valve.VR;
 
-public class LeftController : MonoBehaviour
-{
+public class LeftController : MonoBehaviour {
     public GameObject prefab;
     public Rigidbody attachPoint;
 
@@ -13,36 +12,30 @@ public class LeftController : MonoBehaviour
     SteamVR_Behaviour_Pose trackedObj;
     FixedJoint joint = null;
 
-    private void Awake()
-    {
+    private void Awake() {
         trackedObj = GetComponent<SteamVR_Behaviour_Pose>();
     }
 
-    private void FixedUpdate()
-    {
-        if (joint == null && trigger.GetStateDown(trackedObj.inputSource))
-        {
+    private void FixedUpdate() {
+        if (joint == null && trigger.GetStateDown(trackedObj.inputSource)) {
             GameObject go = Instantiate(prefab);
             go.transform.position = attachPoint.transform.position;
 
             joint = go.AddComponent<FixedJoint>();
             joint.connectedBody = attachPoint;
         }
-        else if (joint != null && trigger.GetStateUp(trackedObj.inputSource))
-        {
+        else if (joint != null && trigger.GetStateUp(trackedObj.inputSource)) {
             GameObject go = joint.gameObject;
             Rigidbody rigidbody = go.GetComponent<Rigidbody>();
             Object.DestroyImmediate(joint);
             joint = null;
             Object.Destroy(go, 15.0f);
             Transform origin = trackedObj.origin ? trackedObj.origin : trackedObj.transform.parent;
-            if (origin != null)
-            {
+            if (origin != null) {
                 rigidbody.velocity = origin.TransformVector(trackedObj.GetVelocity());
                 rigidbody.angularVelocity = origin.TransformVector(trackedObj.GetAngularVelocity());
             }
-            else
-            {
+            else {
                 rigidbody.velocity = trackedObj.GetVelocity();
                 rigidbody.angularVelocity = trackedObj.GetAngularVelocity();
             }
