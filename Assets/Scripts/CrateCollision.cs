7ece46d8b0e6e7fe5f3627b0fb15a9c02f1b215a
Bruno Domingues <brunosd1@al.insper.edu.br>
2022-06-02 17:54:37 -0300

    More edits

--- Assets/Scripts/CrateCollision.cs
+++ Assets/Scripts/CrateCollision.cs
@@ -2,11 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CrateCollision : MonoBehaviour
-{
-    private void OnCollisionEnter(Collision collision)
-    {
-        Debug.Log("collision with " + collision.gameObject.name);
-        if (collision.gameObject.tag == "Sphere") Destroy(gameObject);
+public class CrateCollision : MonoBehaviour {
+    private int life = 3;
+    private void OnCollisionEnter(Collision collision) {
+        Debug.Log("Gcollision with " + collision.gameObject.name);
+        if (collision.gameObject.tag == "Sphere") hit();
     }
+
+    private void hit() {
+        life--;
+        if (life <= 0) Destroy(gameObject);
+    }
+
 }
--- Assets/Scripts/Fireball.cs
+++ Assets/Scripts/Fireball.cs
@@ -2,22 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Fireball : MonoBehaviour
-{
+public class Fireball : MonoBehaviour {
     private static float globalGravity = -6f;
     private Rigidbody rb;
+    private GameObject goblin;
+
+    public Material ice, fire;
+    private bool ballType = false;
 
     // Start is called before the first frame update
-    void OnEnable()
-    {
+    void OnEnable() {
         rb = GetComponent<Rigidbody>();
         rb.useGravity = false;
+        GetComponent<Renderer>().material = ice;
+    }
+
+    void setMaterial() {
+        // set material
+        if (ballType) {
+            GetComponent<Renderer>().material = ice;
+        }
+        else {
+            GetComponent<Renderer>().material = fire;
+        }
+        ballType = !ballType;
     }
 
     // Update is called once per frame
-    void FixedUpdate()
-    {
+    void FixedUpdate() {
         Vector3 gravity = globalGravity * Vector3.up;
         rb.AddForce(gravity, ForceMode.Acceleration);
     }
+
 }
